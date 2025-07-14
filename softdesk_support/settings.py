04338e59b94cf04b3984dbf35bab4851b4f0bf30
Sébastien Grison <74866413+SebGris@users.noreply.github.com>
2025-07-14 16:15:08 +0200

    Ajout de la prise en charge de l'authentification JWT avec djangorestframework-simplejwt et mise à jour des dépendances dans pyproject.toml

--- softdesk_support/settings.py
+++ softdesk_support/settings.py
@@ -38,8 +38,17 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',  # Django REST Framework
+    'rest_framework_simplejwt', # JWT Authentication
     'issues',          # Votre application
 ]
+
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
+    ),
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    'PAGE_SIZE': 100,
+}
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
