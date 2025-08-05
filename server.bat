f5ce0d5f3048b3050f8e027a994cba5bc7312c38
Sébastien Grison <74866413+SebGris@users.noreply.github.com>
2025-08-05 19:58:14 +0200

    feat(server.bat): Ajout d'un lien vers la documentation des codes de statut HTTP pour une meilleure référence

--- server.bat
+++ server.bat
@@ -10,5 +10,6 @@ echo Opening API interface in browser...
 timeout /t 3 /nobreak > nul
 start http://127.0.0.1:8000/admin/
 start https://owasp.org/Top10/
+start https://developer.mozilla.org/en-US/docs/Web/HTTP/Reference/Status/200
 poetry run python manage.py runserver
 pause
